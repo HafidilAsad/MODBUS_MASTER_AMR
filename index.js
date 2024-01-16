5fec2058d3449ee5474ac77a1c727eac62b9c848
Hafidil Asad <hafidilaksad@gmail.com>
2024-01-16 12:22:57 +0700

    update api

--- index.js
+++ index.js
@@ -12,12 +12,53 @@ app.get("/write-modbus-register", (req, res) => {
   const client = new Modbus();
 
   // Define the Modbus server's IP address and port
-  const modbusServerIp = "192.168.1.1";
+  const modbusServerIp = "192.168.137.27";
   const modbusServerPort = 502; // Default Modbus TCP port
 
   // Set the register address and value
   const registerAddress = 10;
   const registerValue = 1;
+
+  // Connect to the Modbus server
+  client.connectTCP(modbusServerIp, { port: modbusServerPort }, (err) => {
+    if (err) {
+      console.error("Error connecting to Modbus server:", err);
+      client.close();
+      res.status(500).send("Error connecting to Modbus server");
+    } else {
+      console.log("Connected to Modbus server");
+
+      // Write a single register with the specified value
+      client.writeRegister(
+        registerAddress,
+        registerValue,
+        (writeErr, response) => {
+          if (writeErr) {
+            console.error("Error writing register:", writeErr);
+            res.status(500).send("Error writing Modbus register");
+          } else {
+            console.log("Write successful:", response);
+            res.status(200).send("Write successful");
+          }
+
+          // Close the connection
+          client.close();
+        }
+      );
+    }
+  });
+});
+app.get("/back-modbus-register", (req, res) => {
+  // Create a Modbus TCP client
+  const client = new Modbus();
+
+  // Define the Modbus server's IP address and port
+  const modbusServerIp = "192.168.137.27";
+  const modbusServerPort = 502; // Default Modbus TCP port
+
+  // Set the register address and value
+  const registerAddress = 10;
+  const registerValue = 0;
 
   // Connect to the Modbus server
   client.connectTCP(modbusServerIp, { port: modbusServerPort }, (err) => {
