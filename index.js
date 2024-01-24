7fb8a7e807b10a1f659b8c25a2fd119f5c20c014
Hafidil Asad <hafidilaksad@gmail.com>
2024-01-24 08:35:56 +0700

    add new mc

--- index.js
+++ index.js
@@ -9,29 +9,76 @@ const app = express();
 app.use(cors());
 const port = process.env.PORT || 5200; // Use the configured port or default to 5100
 
-app.get("/write-modbus-register", (req, res) => {
-  console.log("telah dipanggil lewat api");
-  // Create a Modbus TCP client
-  const client = new Modbus();
+// app.get("/write-modbus-register", (req, res) => {
+//   console.log("telah dipanggil lewat api");
+//   console.log("Pada Jam", new Date().toLocaleString('id-ID'));
+//   // Create a Modbus TCP client
+//   const client = new Modbus();
 
-  // Define the Modbus server's IP address and port
-  const modbusServerIp = "172.172.8.110";
-  const modbusServerPort = 8888; // Default Modbus TCP port
+//   // Define the Modbus server's IP address and port
+//   const modbusServerIp = "172.172.8.110";
+//   const modbusServerPort = 8888; // Default Modbus TCP port
 
-  // Set the register address and value
-  const registerAddress = 10;
-  const registerValue = 1;
+//   // Set the register address and value
+//   const registerAddress = 10;
+//   const registerValue = 1;
 
-  // Connect to the Modbus server
-  client.connectTCP(modbusServerIp, { port: modbusServerPort }, (err) => {
-    if (err) {
-      console.error("Error connecting to Modbus server:", err);
-      client.close();
-      res.status(500).send("Error connecting to Modbus server");
-    } else {
-      console.log("Connected to Modbus server");
+//   // Connect to the Modbus server
+//   client.connectTCP(modbusServerIp, { port: modbusServerPort }, (err) => {
+//     if (err) {
+//       console.error("Error connecting to Modbus server:", err);
+//       client.close();
+//       res.status(500).send("Error connecting to Modbus server");
+//     } else {
+//       console.log("Connected to Modbus server");
 
-      // Write a single register with the specified value
+//       // Write a single register with the specified value
+//       client.writeRegister(
+//         registerAddress,
+//         registerValue,
+//         (writeErr, response) => {
+//           if (writeErr) {
+//             console.error("Error writing register:", writeErr);
+//             res.status(500).send("Error writing Modbus register");
+//           } else {
+//             console.log("Write successful:", response);
+//             res.status(200).send("Write successful");
+//           }
+
+//           // Close the connection
+//           client.close();
+//         }
+//       );
+//     }
+//   });
+// });
+
+const client = new Modbus();
+
+// Define the Modbus server's IP address and port
+const modbusServerIp = "172.172.8.110";
+const modbusServerPort = 8888; // Default Modbus TCP port
+
+// Set the register address and value
+const registerAddress = 10;
+const registerValue = 1;
+
+const registerAddress2 = 11;
+const registerValue2 = 1;
+
+// Connect to the Modbus server
+client.connectTCP(modbusServerIp, { port: modbusServerPort }, (err) => {
+  if (err) {
+    console.error("Error connecting to Modbus server:", err);
+    client.close();
+    // res.status(500).send("Error connecting to Modbus server");
+  } else {
+    console.log("Connected to Modbus server");
+
+    app.get("/write-modbus-register", (req, res) => {
+      console.log("API MC 33 Telah dipanggil");
+      console.log("API call MC 33 completed at:", new Date().toLocaleString('id-ID'));
+
       client.writeRegister(
         registerAddress,
         registerValue,
@@ -42,14 +89,34 @@ app.get("/write-modbus-register", (req, res) => {
           } else {
             console.log("Write successful:", response);
             res.status(200).send("Write successful");
-          }
-
-          // Close the connection
-          client.close();
+          }            
         }
       );
-    }
-  });
+    });
+
+    app.get("/write-modbus-register/2", (req, res) => {
+      console.log("API MC 31 Telah dipanggil");
+      console.log("API MC 31 call completed at:", new Date().toLocaleString('id-ID'));
+
+      client.writeRegister(
+        registerAddress2,
+        registerValue2,
+        (writeErr, response) => {
+          if (writeErr) {
+            console.error("Error writing register MC 31:", writeErr);
+            res.status(500).send("Error writing Modbus register MC 31");
+          } else {
+            console.log("Write successful to MC 31:", response);
+            res.status(200).send("Write successful to MC31");
+          }            
+        }
+      );
+    });
+
+    // Write a single register with the specified value
+  
+  }
+
 });
 
 
